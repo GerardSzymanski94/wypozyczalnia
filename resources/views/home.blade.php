8d5f39c07a9f0435be67dbaca6db2a9d52cca07f
mrchudy <chudymikolaj06@gmail.com>
2020-03-30 08:11:34 +0200

    Usunięcie ostatniej sekcji do podawania ilości produktów

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -241,26 +241,6 @@
             </div>
         </section>
 
-        <section class="MainSection-countItems container">
-            <div class="MainSection-countItems-background"
-                 style="background-image: url('{{ asset('images/wypozyczalnia-rehastore-compex.jpg') }}');">
-                <div class="row justify-content-center">
-                    <div class="MainSection-countItems-group">
-                        {{-- <h3 class="MainSection-countItems--title">Ilość zestawów elektrod</h3>
-                         <label class="MainSection-countItems--amount" for="amount_additional">Ilość zestawów elektrod<span class="required">*</span></label>
-                         <input class="MainSection-countItems--number" type="number" name="amount_additional" id="amount_additional" class="form-control" step="1" value="1" min="1">
-
-                         @if($errors->has('amount_additional'))
-                             <p class="alert alert-danger">
-                                 {{ $errors->first('amount_additional') }}
-                             </p>
-                         @endif--}}
-                    </div>
-                </div>
-            </div>
-        </section>
-
-
         <section class="MainSection-finish-price">
             <div class="container">
                 <div class="row">
