9ecb4508ab0dc139b2cdd34cee96611fabc4f734
Gerard <gerard.szymanski1@gmail.com>
2020-04-02 12:52:40 +0200

    liczenie ceny po zmianie głównego produktu

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -109,19 +109,22 @@
                 <div class="row justify-content-center">
                     <div class="col-12">
                         <div class="MainSection-Items-description">
-                            <h2 class="MainSection-Items--title">Wybierz i zaznacz urządzenie, które chcesz wypożyczyć</h2>
-                            <p  class="MainSection-Items--text">Aby wybrać urządzenie - najedź i kliknij na produkt. Ceny wypożyczania możesz sprawdzić klikając w przycisk "Cennik dla tego modelu".</p>
+                            <h2 class="MainSection-Items--title">Wybierz i zaznacz urządzenie, które chcesz
+                                wypożyczyć</h2>
+                            <p class="MainSection-Items--text">Aby wybrać urządzenie - najedź i kliknij na produkt. Ceny
+                                wypożyczania możesz sprawdzić klikając w przycisk "Cennik dla tego modelu".</p>
                         </div>
                         <div class="@if (count($products) <= 8) single-column @else two-column @endif swiper-container">
                             <div class="swiper-wrapper">
                                 @foreach($products as $product)
                                     <div class="swiper-slide">
-                                        <label class="MainSection-Items-box product_card" for="product_{{ $product->id }}"
-                                            data-id="{{ $product->id }}">
+                                        <label class="MainSection-Items-box product_card"
+                                               for="product_{{ $product->id }}"
+                                               data-id="{{ $product->id }}">
                                             <div id="{{ $product->id }}" class="MainSection-Items--box-height">
                                                 <div class="MainSection-Items-wrapper">
                                                     <img src="{{ asset('storage/'. $product->getMainPhoto->url) }}"
-                                                        class="MainSection-Items--image" alt="{{ $product->name }}">
+                                                         class="MainSection-Items--image" alt="{{ $product->name }}">
                                                     <div class="MainSection-Items-wrapper-description">
                                                         <h5 class="MainSection-Items-wrapper-description--title">{{ $product->name }}</h5>
                                                         <p class="MainSection-Items-wrapper-description--text">Dostępna
@@ -129,7 +132,7 @@
                                                     </div>
 
                                                     <div class="MainSection-Items-collapse collapse fade multi-collapse"
-                                                        id="multiCollapse-{{ $product->id }}">
+                                                         id="multiCollapse-{{ $product->id }}">
                                                         <ul class="MainSection-Items-wrapper-group-list">
                                                             <li class="MainSection-Items-wrapper-group--list">7 dni
                                                                 - {{ $product->price_one_week }}zł /
@@ -156,26 +159,38 @@
                                                         </ul>
                                                     </div>
 
-                                                    <button class="btn btn-product MainSection-Items-wrapper-group--button" type="button" data-toggle="collapse"
-                                                        data-target="#multiCollapse-{{ $product->id }}"
-                                                        aria-expanded="false"
-                                                        aria-controls="multiCollapse-{{ $product->id }}">Cennik dla tego modelu
+                                                    <button class="btn btn-product MainSection-Items-wrapper-group--button"
+                                                            type="button" data-toggle="collapse"
+                                                            data-target="#multiCollapse-{{ $product->id }}"
+                                                            aria-expanded="false"
+                                                            aria-controls="multiCollapse-{{ $product->id }}">Cennik dla
+                                                        tego modelu
                                                     </button>
                                                 </div>
                                             </div>
                                         </label>
                                     </div>
                                     <input type="checkbox" name="product" class="product_checkbox"
-                                        value="{{ $product->id }}" style="display: none;"
-                                        id="product_{{ $product->id }}">
+                                           value="{{ $product->id }}" style="display: none;"
+                                           id="product_{{ $product->id }}">
                                 @endforeach
                             </div>
 
                             <div class="swiper-button-prev">
-                                <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="angle-left" class="swiper-button-prev-icon svg-inline--fa fa-angle-left fa-w-8" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512"><path fill="currentColor" d="M31.7 239l136-136c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9L127.9 256l96.4 96.4c9.4 9.4 9.4 24.6 0 33.9L201.7 409c-9.4 9.4-24.6 9.4-33.9 0l-136-136c-9.5-9.4-9.5-24.6-.1-34z"></path></svg>
+                                <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="angle-left"
+                                     class="swiper-button-prev-icon svg-inline--fa fa-angle-left fa-w-8" role="img"
+                                     xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512">
+                                    <path fill="currentColor"
+                                          d="M31.7 239l136-136c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9L127.9 256l96.4 96.4c9.4 9.4 9.4 24.6 0 33.9L201.7 409c-9.4 9.4-24.6 9.4-33.9 0l-136-136c-9.5-9.4-9.5-24.6-.1-34z"></path>
+                                </svg>
                             </div>
                             <div class="swiper-button-next">
-                                <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="angle-right" class="swiper-button-prev-icon svg-inline--fa fa-angle-right fa-w-8" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512"><path fill="currentColor" d="M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34z"></path></svg>
+                                <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="angle-right"
+                                     class="swiper-button-prev-icon svg-inline--fa fa-angle-right fa-w-8" role="img"
+                                     xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512">
+                                    <path fill="currentColor"
+                                          d="M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34z"></path>
+                                </svg>
                             </div>
                         </div>
                     </div>
@@ -191,15 +206,16 @@
                         <div class="row">
                             <div class="col-12 col-md-6 mb-4 mb-md-0">
                                 <div class="MainSection-countItems-group-wrapper">
-                                   <div class="MainSection-countItems-group-wrapper-space">
+                                    <div class="MainSection-countItems-group-wrapper-space">
                                         <h3 class="MainSection-countItems--title">Ilość urządzeń</h3>
                                         <label class="MainSection-countItems--amount" for="amount">Ilość urządzeń
                                             <span class="required">*</span>
                                         </label>
-                                   </div>
+                                    </div>
 
-                                    <input class="MainSection-countItems--number" type="number" name="amount" id="amount"
-                                    step="1" value="1" min="1">
+                                    <input class="MainSection-countItems--number" type="number" name="amount"
+                                           id="amount"
+                                           step="1" value="1" min="1">
                                 </div>
 
                                 @if($errors->has('amount'))
@@ -212,13 +228,14 @@
                             <div class="col-12 col-md-6">
                                 <div class="MainSection-countItems-group-wrapper">
                                     <div class="MainSection-countItems-group-wrapper-space">
-                                        <h3 class="MainSection-countItems--title">Wybierz liczbę dni na jaką chcesz wypożyczyć urządzenie</h3>
+                                        <h3 class="MainSection-countItems--title">Wybierz liczbę dni na jaką chcesz
+                                            wypożyczyć urządzenie</h3>
                                         <label class="MainSection-countItems--amount" for="days">Liczba dni wypożyczenia
                                             <span class="required">*</span>
                                         </label>
                                     </div>
                                     <input class="MainSection-countItems--number form-control" type="number"
-                                    name="days" id="days" step="1" value="1" min="1">
+                                           name="days" id="days" step="1" value="1" min="1">
                                 </div>
 
                                 @if($errors->has('days'))
@@ -236,19 +253,23 @@
                 <div class="row justify-content-center">
                     <div class="col-md-12">
                         <div class="MainSection-Items-description">
-                            <h2 class="MainSection-Items--title">Do korzystania z elektrostymulatora konieczne są elektrody. Można także skorzystać z dodatkówych akcesoriów. Akcesoria i elektrody możesz kupić w tym kroku. Przygotowaliśmy take zestawy elektrod pod konkretne potrzeby.</h2>
-                            <p  class="MainSection-Items--text">Zaznacz produkty, które chcesz dobrać do wypożyczonego urządzenia.</p>
+                            <h2 class="MainSection-Items--title">Do korzystania z elektrostymulatora konieczne
+                                są elektrody. Można także skorzystać z dodatkówych akcesoriów. Akcesoria i elektrody
+                                możesz kupić w tym kroku. Przygotowaliśmy take zestawy elektrod pod konkretne
+                                potrzeby.</h2>
+                            <p class="MainSection-Items--text">Zaznacz produkty, które chcesz dobrać do wypożyczonego
+                                urządzenia.</p>
                         </div>
                         <div class="@if (count($additionals) <= 8) single-column @else two-column @endif swiper-container">
                             <div class="swiper-wrapper">
                                 @foreach($additionals as $additional)
                                     <div class="swiper-slide">
                                         <label class="MainSection-Items-box additional_card"
-                                            for="additional_{{ $additional->id }}" data-id="{{ $additional->id }}">
+                                               for="additional_{{ $additional->id }}" data-id="{{ $additional->id }}">
                                             <div id="{{ $additional->id }}" class="MainSection-Items--box-height">
                                                 <div class="MainSection-Items-wrapper">
                                                     <img src="{{ asset('storage/'. $additional->getMainPhoto->url) }}"
-                                                        class="MainSection-Items--image" alt="{{ $additional->name }}">
+                                                         class="MainSection-Items--image" alt="{{ $additional->name }}">
 
                                                     <div class="MainSection-Items-wrapper-description">
                                                         <h5 class="MainSection-Items-wrapper-description--title">{{ $additional->name }}</h5>
@@ -264,9 +285,11 @@
                                                         <li>
                                                             <label class="MainSection-Items-wrapper-group--label">
                                                                 <span>Ilość:</span>
-                                                                <input class="MainSection-Items-wrapper-group--countItems-number" type="number"
-                                                                    name="amount_additional[{{ $additional->id }}]" id="amount_additional"
-                                                                    class="form-control" step="1" value="1" min="1">
+                                                                <input class="MainSection-Items-wrapper-group--countItems-number"
+                                                                       type="number"
+                                                                       name="amount_additional[{{ $additional->id }}]"
+                                                                       id="amount_additional"
+                                                                       class="form-control" step="1" value="1" min="1">
                                                             </label>
                                                         </li>
                                                     </ul>
@@ -274,17 +297,27 @@
                                             </div>
                                         </label>
                                         <input type="checkbox" name="additional[{{ $additional->id }}]"
-                                            class="additional_checkbox" style="display: none;"
-                                            id="additional_{{ $additional->id }}" value="{{ $additional->id }}">
+                                               class="additional_checkbox" style="display: none;"
+                                               id="additional_{{ $additional->id }}" value="{{ $additional->id }}">
                                     </div>
                                 @endforeach
                             </div>
 
                             <div class="swiper-button-prev">
-                                <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="angle-left" class="swiper-button-prev-icon svg-inline--fa fa-angle-left fa-w-8" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512"><path fill="currentColor" d="M31.7 239l136-136c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9L127.9 256l96.4 96.4c9.4 9.4 9.4 24.6 0 33.9L201.7 409c-9.4 9.4-24.6 9.4-33.9 0l-136-136c-9.5-9.4-9.5-24.6-.1-34z"></path></svg>
+                                <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="angle-left"
+                                     class="swiper-button-prev-icon svg-inline--fa fa-angle-left fa-w-8" role="img"
+                                     xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512">
+                                    <path fill="currentColor"
+                                          d="M31.7 239l136-136c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9L127.9 256l96.4 96.4c9.4 9.4 9.4 24.6 0 33.9L201.7 409c-9.4 9.4-24.6 9.4-33.9 0l-136-136c-9.5-9.4-9.5-24.6-.1-34z"></path>
+                                </svg>
                             </div>
                             <div class="swiper-button-next">
-                                <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="angle-right" class="swiper-button-prev-icon svg-inline--fa fa-angle-right fa-w-8" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512"><path fill="currentColor" d="M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34z"></path></svg>
+                                <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="angle-right"
+                                     class="swiper-button-prev-icon svg-inline--fa fa-angle-right fa-w-8" role="img"
+                                     xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512">
+                                    <path fill="currentColor"
+                                          d="M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34z"></path>
+                                </svg>
                             </div>
                         </div>
                     </div>
@@ -346,6 +379,9 @@
         $('body').on('change', '#amount_additional', function () {
             getPrice();
         });
+        $('body').on('change', '.product_card', function () {
+            getPrice();
+        });
 
         $('body').on('click', '#add_product', function (e) {
             e.preventDefault();
@@ -396,7 +432,7 @@
                     $('html, body').animate({
                         scrollTop: $("body").offset().top
                     }, '250', () => {
-                        $('.MainSection-alerts-close').on( 'click', () => {
+                        $('.MainSection-alerts-close').on('click', () => {
                             $('.check_alert').hide();
                         })
                     });
