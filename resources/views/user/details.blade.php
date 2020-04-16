61b2d0f6261f9a2cda14604baacb09550064c110
Gerard <gerard.szymanski1@gmail.com>
2020-04-16 09:19:22 +0200

    poprawa wyświetlania szczegółów zamówienia

--- resources/views/user/details.blade.php
+++ resources/views/user/details.blade.php
@@ -1,66 +1,80 @@
 @extends('layouts.app')
 
 @section('content')
-<section class="MainSection-details-order container">
-    <div class="row justify-content-center">
-        <div class="col-12">
-            <div class="MainSection-details-order-panel">
-                <div class="MainSection-details-order-header">
-                    <h2 class="MainSection-details-order-header--title">Szczegóły zamówienia</h2>
-                </div>
-
-                <div class="MainSection-details-order_content">
-                    <div class="MainSection-details-order-user-adress">
-                        <h2 class="MainSection-details-order-user--title" >Dane użytkownika</h2>
-                        <ul class="MainSection-details-order-user--list">
-                            <li class="MainSection-details-order-user--text">Email: {{ $order->user->email }}</li>
-                            <li class="MainSection-details-order-user--text">Imię: {{ $order->user->name }}</li>
-                            <li class="MainSection-details-order-user--text">Nazwisko: {{ $order->user->surname }}</li>
-                            <li class="MainSection-details-order-user--text">Adres: {{ $order->user->street }}, {{ $order->user->city }}, {{ $order->user->zip_code }}</li>
-                        </ul>
+    <section class="MainSection-details-order container">
+        <div class="row justify-content-center">
+            <div class="col-12">
+                <div class="MainSection-details-order-panel">
+                    <div class="MainSection-details-order-header">
+                        <h2 class="MainSection-details-order-header--title">Szczegóły zamówienia</h2>
                     </div>
 
-                    <div class="MainSection-details-order-panel">
-                        <div class="MainSection-details-order-content">
-                            <ul class="MainSection-details-order-table">
-                                <li class="MainSection-details-order-table-row MainSection-details-order-table-cell-header">
-                                    <div class="MainSection-details-order-table-cell MainSection-details-order-table--product">Produkt</div>
-                                    <div class="MainSection-details-order-table-cell MainSection-details-order-table--count">Ilość</div>
-                                    <div class="MainSection-details-order-table-cell MainSection-details-order-table--price">Cena</div>
-                                    <div class="MainSection-details-order-table-cell MainSection-details-order-table--date">Data oddania</div>
-                                    <div class="MainSection-details-order-table-cell MainSection-details-order-table--status">Status</div>
-                                </li>
-
-                                @foreach($order->orderProducts as $orderProduct)
-                                <li class="MainSection-details-order-table-row">
-                                    <div class="MainSection-details-order-table-cell MainSection-details-order-table--product">
-                                        <div class="MainSection-details-order-table-cell-name">Produkt</div>
-                                        <span>{{ $orderProduct->product->name }}</span>
-                                    </div>
-                                    <div class="MainSection-details-order-table-cell MainSection-details-order-table--count">
-                                        <div class="MainSection-details-order-table-cell-name">Ilość</div>
-                                        <span>{{ $orderProduct->amount }}</span>
-                                    </div>
-                                    <div class="MainSection-details-order-table-cell MainSection-details-order-table--price">
-                                        <div class="MainSection-details-order-table-cell-name">Cena</div>
-                                        <span>   {{ $orderProduct->price }} zł</span>
-                                    </div>
-                                    <div class="MainSection-details-order-table-cell MainSection-details-order-table--date">
-                                        <div class="MainSection-details-order-table-cell-name">Data oddania</div>
-                                        <span>{{ $orderProduct->showDay() }}</span>
-                                    </div>
-                                    <div class="MainSection-details-order-table-cell MainSection-details-order-table--status">
-                                        <div class="MainSection-details-order-table-cell-name">Status</div>
-                                        <span>{{ $orderProduct->showStatus() }}</span>
-                                    </div>
-                                </li>
-                                @endforeach
+                    <div class="MainSection-details-order_content">
+                        <div class="MainSection-details-order-user-adress">
+                            <h2 class="MainSection-details-order-user--title">Dane użytkownika</h2>
+                            <ul class="MainSection-details-order-user--list">
+                                <li class="MainSection-details-order-user--text">Email: {{ $order->user->email }}</li>
+                                <li class="MainSection-details-order-user--text">Imię: {{ $order->user->name }}</li>
+                                <li class="MainSection-details-order-user--text">
+                                    Nazwisko: {{ $order->user->surname }}</li>
+                                <li class="MainSection-details-order-user--text">Adres: {{ $order->user->street }}
+                                    , {{ $order->user->city }}, {{ $order->user->zip_code }}</li>
                             </ul>
+                        </div>
+
+                        <div class="MainSection-details-order-panel">
+                            <div class="MainSection-details-order-content">
+                                <ul class="MainSection-details-order-table">
+                                    <li class="MainSection-details-order-table-row MainSection-details-order-table-cell-header">
+                                        <div class="MainSection-details-order-table-cell MainSection-details-order-table--product">
+                                            Produkt
+                                        </div>
+                                        <div class="MainSection-details-order-table-cell MainSection-details-order-table--count">
+                                            Ilość
+                                        </div>
+                                        <div class="MainSection-details-order-table-cell MainSection-details-order-table--price">
+                                            Cena
+                                        </div>
+                                        <div class="MainSection-details-order-table-cell MainSection-details-order-table--date">
+                                            Data oddania
+                                        </div>
+                                        <div class="MainSection-details-order-table-cell MainSection-details-order-table--status">
+                                            Status
+                                        </div>
+                                    </li>
+
+                                    @foreach($order->orderProducts as $orderProduct)
+                                        <li class="MainSection-details-order-table-row">
+                                            <div class="MainSection-details-order-table-cell MainSection-details-order-table--product">
+                                                <div class="MainSection-details-order-table-cell-name">Produkt</div>
+                                                <span>{{ $orderProduct->product->name }}</span>
+                                            </div>
+                                            <div class="MainSection-details-order-table-cell MainSection-details-order-table--count">
+                                                <div class="MainSection-details-order-table-cell-name">Ilość</div>
+                                                <span>@if($orderProduct->product->status==1){{ $orderProduct->amount }}@else
+                                                        {{ $orderProduct->amount_additional }}@endif</span>
+                                            </div>
+                                            <div class="MainSection-details-order-table-cell MainSection-details-order-table--price">
+                                                <div class="MainSection-details-order-table-cell-name">Cena</div>
+                                                <span>   {{ $orderProduct->price }} zł</span>
+                                            </div>
+                                            <div class="MainSection-details-order-table-cell MainSection-details-order-table--date">
+                                                <div class="MainSection-details-order-table-cell-name">Data oddania
+                                                </div>
+                                                <span>{{ $orderProduct->showDay() }}</span>
+                                            </div>
+                                            <div class="MainSection-details-order-table-cell MainSection-details-order-table--status">
+                                                <div class="MainSection-details-order-table-cell-name">Status</div>
+                                                <span>{{ $orderProduct->showStatus() }}</span>
+                                            </div>
+                                        </li>
+                                    @endforeach
+                                </ul>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-</section>
+    </section>
 @endsection
